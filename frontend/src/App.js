255395dc86d911010ee2bb2c51e4dfbf770bfbad
Ksenia <nikkseniyu@gmail.com>
2025-09-23 19:03:34 +0700

    add in html elements
    add elemets website

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,11 +6,17 @@ function App() {
         <div className="app-content">
             <div className="black-stripe"></div>
             <nav className="navigation-block">
-                <h1>Aesthetic Furniture</h1>
-                <p>Современная эстетичная мебель</p>
+                <h1 className="name-website">Aesthetic Furniture</h1>
+                <button className="button-navigation" aria-label="Create project">
+                    <span className="button-text">Create project</span>
+                </button>
             </nav>
-            <section className="background-info">
+            <section className="background-info"></section>
+            <section className="block-info">
+                <section className="background-description">
 
+                </section>
+                <div className="image-object"></div>
             </section>
         </div>
     );
