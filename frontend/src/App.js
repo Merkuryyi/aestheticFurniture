d1dc7dfdae302d33f5d02a119183e350ee2f5cf3
Ksenia <nikkseniyu@gmail.com>
2025-09-23 16:10:29 +0700

    Initial commit
    Create project

---
+++ frontend/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './styles/index.css';
+
+function App() {
+    return (
+        <div className="app-content">
+            <div className="black-stripe"></div>
+            <nav className="navigation-block">
+                <h1>Aesthetic Furniture</h1>
+                <p>Современная эстетичная мебель</p>
+            </nav>
+            <section className="background-info">
+
+            </section>
+        </div>
+    );
+}
+
+export default App;
