db9cd9aba7e869bbaf7eb126c56abe1c01ac4e53
Ksenia <nikkseniyu@gmail.com>
2025-09-26 15:55:02 +0700

    images changed
    images have been changed and I add new section.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,10 +4,11 @@ import Header from './components/Header/Header';
 import SectionInfoWebSite from './components/SectionInfoWebSite/SectionInfoWebSite';
 import BlockInfo from "./components/BlockInfo/BlockInfo";
 import FooterPanel from "./components/FooterPanel/FooterPanel";
-import lampsImage from './images/lamps.jpg';
-import sofaLampImage from './images/sofaLamp.jpg';
-import backgroundImage from './images/objects.jpg';
+import armchairImage from './images/armchair.png';
+import lampImage from './images/lamp.png';
+import backgroundImage from './images/background-block.png';
 import modernBackgroundImage from './images/modern-room.jpeg';
+import AboutCompany from "./components/AboutCompany/AboutCompany";
 function App() {
     return (
         <div className="app-content">
@@ -15,26 +16,27 @@ function App() {
             <Header />
             <SectionInfoWebSite
                 imageSrc={backgroundImage}
-                title="Эстетичная мебель"
+                title="Эстетичная мебель - инималистичные и трендовые предметы интерьера, которые создадут гармонию и вдохновение в любой комнате."
             />
             <BlockInfo
                 title="Современная эстетика для вашего дома"
                 description="Красота, комфорт и стиль в каждой детали. Вдохновляйтесь свежими интерьерными решениями каждый день."
                 buttonText="View collection"
-                imageSrc={lampsImage}
+                imageSrc={armchairImage}
                 reverse={false}
             />
             <BlockInfo
                 title="Идеальная мебель для уютной жизни"
                 description="У нас вы найдете минималистичные и трендовые предметы интерьера, которые создадут гармонию и вдохновение в любой комнате."
                 buttonText="View collection"
-                imageSrc={sofaLampImage}
+                imageSrc={lampImage}
                 reverse={true}
             />
             <SectionInfoWebSite
                 imageSrc={modernBackgroundImage}
                 title="Соберите уникальный образ комнаты — наши дизайнерские подборки помогут быстро подобрать гармоничные сочетания мебели и аксессуаров."
             />
+            <AboutCompany />
             <FooterPanel />
 
 
---
+++ frontend/src/components/AboutCompany/AboutCompany.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './AboutCompany.css';
+
+function AboutCompany() {
+
+    return (
+        <div className="about-company">
+
+        </div>
+
+    );
+}
+
+export default AboutCompany;
