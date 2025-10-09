9d63ac0e93b9ceb319caa11fd96d87e1a87b896e
Ksenia <nikkseniyu@gmail.com>
2025-10-09 21:33:20 +0700

    build images

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,23 +4,18 @@ import Header from './components/Header/Header';
 import SectionInfoWebSite from './components/SectionInfoWebSite/SectionInfoWebSite';
 import BlockInfo from "./components/BlockInfo/BlockInfo";
 import FooterPanel from "./components/FooterPanel/FooterPanel";
-import armchairImage from './images/armchair.png';
-import lampImage from './images/lamp.png';
-import backgroundImage from './images/background-block.png';
-import modernBackgroundImage from './images/modern-room.jpeg';
 import AboutCompany from "./components/AboutCompany/AboutCompany";
 
 function App() {
     return (
         <div className="app-content">
-
             <main className="snap-container">
                 <section className="snap-section" id="section">
                     <Header />
                 </section>
                 <section className="snap-section" id="section1">
                     <SectionInfoWebSite
-                        imageSrc={backgroundImage}
+                        imageSrc="/images/background-block.png"
                         title="Эстетичная мебель - минималистичные и трендовые предметы интерьера, которые создадут гармонию и вдохновение в любой комнате."
                     />
                 </section>
@@ -29,7 +24,7 @@ function App() {
                     <BlockInfo
                         title="Современная эстетика для вашего дома"
                         description="Красота, комфорт и стиль в каждой детали. Вдохновляйтесь свежими интерьерными решениями каждый день."
-                        imageSrc={armchairImage}
+                        imageSrc="/images/armchair.png"
                         reverse={false}
                     />
                 </section>
@@ -37,15 +32,15 @@ function App() {
                 <section className="snap-section" id="block2">
                     <BlockInfo
                         title="Идеальная мебель для уютной жизни"
-                        description="У нас вы найдете минималистичные и трендовые предметы интерьера, которые создаруют гармонию и вдохновение в любой комнате."
-                        imageSrc={lampImage}
+                        description="У нас вы найдете минималистичные и трендовые предметы интерьера, которые создают гармонию и вдохновение в любой комнате."
+                        imageSrc="/images/lamp.png"
                         reverse={true}
                     />
                 </section>
 
                 <section className="snap-section" id="section2">
                     <SectionInfoWebSite
-                        imageSrc={modernBackgroundImage}
+                        imageSrc="/images/modern-room.jpeg"
                         title="Соберите уникальный образ комнаты — наши дизайнерские подборки помогут быстро подобрать гармоничные сочетания мебели и аксессуаров."
                     />
                 </section>
